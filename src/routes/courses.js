e128493106e70176f3c4339af4a19ae03b4d7b45
hoangtatthanh <tatthanh0304@gmail.com>
2022-02-20 17:16:45 +0700

    soft delete, install mongoose-delete

--- src/routes/courses.js
+++ src/routes/courses.js
@@ -8,6 +8,8 @@ router.post('/store', courseController.store);
 router.get('/:id/edit', courseController.edit);
 router.put('/:id', courseController.update);
 router.delete('/:id', courseController.delete);
+router.patch('/:id/restore', courseController.restore);
+router.delete('/:id/force', courseController.forceDelete);
 router.get('/:slug', courseController.showDetail);
 
 module.exports = router;
